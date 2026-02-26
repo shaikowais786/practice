c611f80cb3e5c9114fc4d0fe1816de1e4973e067
shaikowais786 <245123737171@mvsrec.edu.in>
2026-02-26 05:53:59 +0530

    Create owais.java

---
+++ owais.java
@@ -0,0 +1,16 @@
+class biggest{
+  public static void main(String args[]){
+    int n1=Integer.parseInt(args[0]);
+    int n2=Integer.paeseInt(args[1]);
+    int n3=Integer.parseInt(args[2]);
+  if((n1>n2)&&(n1>n3)){
+    System.out.println(n1 +"n1 is biggest");
+  }
+  else if((n2>n3)&&(n2>n1)){
+    System.out.println(n2 +"n2 is biggest");
+  }
+   else{
+     System.out.println(n3 + "n3 is biggest");
+   }
+  }
+}
